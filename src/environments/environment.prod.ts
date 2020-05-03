8659c639cb015db2768982d7424836b8d03e6574
jleon253 <jleon253@gmail.com>
2020-05-03 12:16:23 -0500

    Generando deploy para github pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyBT95s7plWXhxoL0lcDTHkS4Zhx0lYGBao',
+    authDomain: 'firechat-angular-37a95.firebaseapp.com',
+    databaseURL: 'https://firechat-angular-37a95.firebaseio.com',
+    projectId: 'firechat-angular-37a95',
+    storageBucket: 'firechat-angular-37a95.appspot.com',
+    messagingSenderId: '459214871628',
+    appId: '1:459214871628:web:e89d06c001960c67d228e9'
+  }
 };
