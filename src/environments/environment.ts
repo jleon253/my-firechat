4c98b0b07b7cd238b35523d4487a18d2b1ff4e34
John Edward Leon Muñoz <jleon253@gmail.com>
2024-04-16 14:14:33 -0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: 'AIzaSyBT95s7plWXhxoL0lcDTHkS4Zhx0lYGBao',
+    apiKey: '***',
     authDomain: 'firechat-angular-37a95.firebaseapp.com',
     databaseURL: 'https://firechat-angular-37a95.firebaseio.com',
-    projectId: 'firechat-angular-37a95',
+    projectId: '***',
     storageBucket: 'firechat-angular-37a95.appspot.com',
-    messagingSenderId: '459214871628',
-    appId: '1:459214871628:web:e89d06c001960c67d228e9'
+    messagingSenderId: '***',
+    appId: '***'
   }
 };
 
